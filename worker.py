d931406981557c8c4b8fd8f81b4f0ea0b39b7d67
unknown <p.plue1881@gmail.com>
2024-10-07 20:46:00 +0900

    refactor: 워커 모듈 리팩토링

--- worker.py
+++ worker.py
@@ -1,12 +1,19 @@
 import redis
 from rq import Worker, Queue, Connection
 from recommender import update_model
+
 redis_conn = redis.Redis()
 
 def update_data(data):
-    
-    update_model(data['gender'],data['age'],data['color'],data['faceshape'],data['bodyshape'],data['clothes'],int(data['rating']))
-
+    update_model(
+        data['gender'],
+        data['age'],
+        data['color'],
+        data['faceshape'],
+        data['bodyshape'],
+        data['clothes'],
+        int(data['rating'])
+    )
 
 if __name__ == '__main__':
     with Connection(redis_conn):
