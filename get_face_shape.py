3487340ec3f2e5235f363279bb1b9ea285e30899
unknown <p.plue1881@gmail.com>
2024-10-07 19:30:08 +0900

    refactor: 얼굴형 추출 모듈 리팩토링

--- get_face_shape.py
+++ get_face_shape.py
@@ -1,47 +1,39 @@
 import tensorflow as tf
 import cv2
 import numpy as np
+
 interpreter = tf.lite.Interpreter(model_path='model/faceshape_efficientnetb4_crop.tflite')
 interpreter.allocate_tensors()
 
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
-faceshape = ['heart','oblong','oval','round','square']
+FACES_SHAPES = ['heart','oblong','oval','round','square']
 face_cascade = cv2.CascadeClassifier('./model/haarcascade_frontalface_default.xml')
-def get_face_shape(faceimage):
-    
-    
+
+def get_face_shape(face_image):
     try:
-        gray = cv2.cvtColor(faceimage,cv2.COLOR_BGR2GRAY)
-    except:
+        gray = cv2.cvtColor(face_image, cv2.COLOR_BGR2GRAY)
+    except cv2.error:
         return None
+
     faces = face_cascade.detectMultiScale(gray)
-    if len(faces)>0:
-        x,y,w,h = faces[0]
-    else:
+    if len(faces) == 0:
         return None
+    x, y, w, h = faces[0]
 
-    img = cv2.cvtColor(faceimage,cv2.COLOR_BGR2RGB)
-    img = img[y:y+h,x:x+w]
-    img = cv2.resize(img,(380,380))
+    img_rgb = cv2.cvtColor(face_image,cv2.COLOR_BGR2RGB)
+    face_crop = img_rgb[y:y+h,x:x+w]
+    face_resized = cv2.resize(face_crop,(380,380))
 
-    arr = np.array(img)
-
+    img_array = np.array(face_resized)
     mean = [159.80679614507162,122.67351405273274,104.61857584263046]
     std = [72.58244780862275,62.41943811258287,59.047168710327774]
+    img_normalized = (img_array - mean) / std
+    img_expanded = np.expand_dims(img_normalized, axis=0)
 
-    for i in range(3):
-        arr[:,:,i] = (arr[:,:,i].astype(np.float32) - mean[i])/std[i]
-    
-    arr = np.expand_dims(arr,axis=0)
-    interpreter.set_tensor(input_details[0]['index'], arr.astype(np.float32))
-
-    # 추론 실행
+    interpreter.set_tensor(input_details[0]['index'], img_expanded.astype(np.float32))
     interpreter.invoke()
-
-    # 출력 텐서에서 결과 가져오기
     result = interpreter.get_tensor(output_details[0]['index'])
-    #print("'Heart': 0, 'Oblong': 1, 'Oval': 2, 'Round': 3,'Square':4")
-    #print(result[0])
-    return faceshape[result[0].argmax()]
+    face_shape_index = np.argmax(result[0])
+    return FACES_SHAPES[face_shape_index]
